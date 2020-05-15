b074d1172f1748e3c484b84422c1d608d3168d77
poetdeaths <poetdeaths@gmail.com>
2020-05-15 16:07:02 +0900

    프론트 커밋

--- src/App.vue
+++ src/App.vue
@@ -5,56 +5,37 @@
       color="primary"
       dark
     >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
+      <v-app-bar-nav-icon @click="drawer = !drawer" />
+      <site-title :title="title"></site-title>
     </v-app-bar>
-
+    <v-navigation-drawer app v-model="drawer">
+    <site-menu></site-menu>
+    </v-navigation-drawer>
     <v-content>
-      <HelloWorld/>
+      <router-view/>
     </v-content>
+    <site-footer :footer="footer"></site-footer>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
-
+import SiteTitle from '@/components/site/title'
+import SiteFooter from '@/components/site/footer'
+import SiteMenu from '@/components/site/menu'
 export default {
-  name: 'App',
-
   components: {
-    HelloWorld
+    SiteTitle,
+    SiteFooter,
+    SiteMenu
   },
-
-  data: () => ({
-    //
-  })
+  name: 'App',
+  data () {
+    return {
+      drawer: false,
+      items: [],
+      title: '나의 타이틀 입니다.',
+      footer: '푸터입니다.'
+    }
+  }
 }
 </script>
---
+++ src/components/site/footer.vue
@@ -0,0 +1,14 @@
+<template>
+<v-footer app color="primary" dark absolute>
+    <v-spacer/>
+    <div>&copy; {{ new Date().getFullYear() + ' ' + footer }}</div>
+</v-footer>
+</template>
+<script>
+export default {
+  props: ['footer']
+}
+</script>
+<style>
+
+</style>
